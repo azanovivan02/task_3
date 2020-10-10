407918ba24b5c670f36b09fc12d2b426473de06d
Azanov, Ivan <ivan.azanov@alibaba-inc.com>
2020-10-10 21:22:08 +0300

    Added tests for subscription

--- backend/src/main/java/ru/made/flitter/FlitRestController.java
+++ backend/src/main/java/ru/made/flitter/FlitRestController.java
@@ -16,51 +16,81 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.emptySet;
 
 @RestController
 @RequestMapping("flit")
 public class FlitRestController {
 
-	@Autowired
-	StateHolder holder;
+    @Autowired
+    StateHolder holder;
 
-	@PostMapping("/add")
-	public ResponseEntity<Boolean> add(@RequestBody AddFlitForm form) {
-		String userToken = form.getUserToken();
-		if (!holder.getTokenToUserMap().containsKey(userToken)) {
-			return ResponseEntity.badRequest().body(false);
-		}
+    @PostMapping("/add")
+    public ResponseEntity<Boolean> add(@RequestBody AddFlitForm form) {
+        String userToken = form.getUserToken();
+        if (!holder.getTokenToUserMap().containsKey(userToken)) {
+            return ResponseEntity.badRequest().body(false);
+        }
 
-		User user = holder.getTokenToUserMap().get(userToken);
-		String userName = user.getUserName();
-		String content = form.getContent();
-		Flit newFlit = new Flit(userName, content);
+        User user = holder.getTokenToUserMap().get(userToken);
+        String userName = user.getUserName();
+        String content = form.getContent();
+        Flit newFlit = new Flit(userName, content);
 
-		List<Flit> userFlits = holder.getNameToFlitsMap().computeIfAbsent(userName, e -> new ArrayList<>());
-		userFlits.add(newFlit);
+        List<Flit> userFlits = holder.getNameToFlitsMap().computeIfAbsent(userName, e -> new ArrayList<>());
+        userFlits.add(newFlit);
 
-		return ResponseEntity.ok(true);
-	}
+        return ResponseEntity.ok(true);
+    }
 
-	@GetMapping("list")
-	public ResponseEntity<List<Flit>> listAllFlits() {
-		List<Flit> allFlits = holder.getNameToFlitsMap()
-				.entrySet()
+    @GetMapping("list")
+    public ResponseEntity<List<Flit>> listAllFlits() {
+        List<Flit> allFlits = holder.getNameToFlitsMap()
+                .entrySet()
+                .stream()
+                .sorted(Comparator.comparing(Map.Entry::getKey))
+                .map(Map.Entry::getValue)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(allFlits);
+    }
+
+    @GetMapping("list/{userName}")
+    public ResponseEntity<List<Flit>> listFlitsPublishedUser(@PathVariable String userName) {
+        List<Flit> userFlits = holder.getNameToFlitsMap().getOrDefault(userName, emptyList());
+        return ResponseEntity.ok(userFlits);
+    }
+
+    @GetMapping("list/feed/{userToken}")
+    public ResponseEntity<List<Flit>> listFlitsConsumedByUser(@PathVariable String userToken) {
+        if (!holder.getTokenToUserMap().containsKey(userToken)) {
+            return ResponseEntity.badRequest().body(emptyList());
+        }
+
+        var userName = holder.getTokenToUserMap()
+                .get(userToken)
+                .getUserName();
+        var publishers = holder
+                .getSubscriberNameToPublishersMap()
+                .getOrDefault(userName, emptySet())
 				.stream()
-				.sorted(Comparator.comparing(Map.Entry::getKey))
-				.map(Map.Entry::getValue)
-				.flatMap(Collection::stream)
+				.sorted()
 				.collect(Collectors.toList());
 
-		return ResponseEntity.ok(allFlits);
-	}
+		var consumedFlits = publishers
+				.stream()
+				.map(name -> holder.getNameToFlitsMap().getOrDefault(name, emptyList()))
+				.flatMap(List::stream)
+				.collect(Collectors.toList());
 
-	@GetMapping("list/{userName}")
-	public ResponseEntity<List<Flit>> listFlitsForUser(@PathVariable String userName) {
-		List<Flit> userFlits = holder.getNameToFlitsMap().getOrDefault(userName, Collections.emptyList());
-		return ResponseEntity.ok(userFlits);
-	}
+        return ResponseEntity.ok(consumedFlits);
+    }
 }
---
+++ backend/src/test/java/ru/made/flitter/utils/FlitterRestWrapper.java
@@ -0,0 +1,155 @@
+package ru.made.flitter.utils;
+
+import org.springframework.boot.test.web.client.TestRestTemplate;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static ru.made.flitter.utils.CollectionTestUtils.castToMap;
+import static ru.made.flitter.utils.CollectionTestUtils.castToMaps;
+import static ru.made.flitter.utils.TestConstants.CONTENT;
+import static ru.made.flitter.utils.TestConstants.LOCALHOST;
+import static ru.made.flitter.utils.TestConstants.PUBLISHER_NAME;
+import static ru.made.flitter.utils.TestConstants.SUBSCRIBER_TOKEN;
+import static ru.made.flitter.utils.TestConstants.USER_NAME;
+import static ru.made.flitter.utils.TestConstants.USER_TOKEN;
+
+public class FlitterRestWrapper {
+
+    private final TestRestTemplate restTemplate;
+    private final int port;
+
+    public FlitterRestWrapper(TestRestTemplate restTemplate, int port) {
+        this.restTemplate = restTemplate;
+        this.port = port;
+    }
+
+    public TestRestTemplate getRestTemplate() {
+        return restTemplate;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void clear() {
+        var endpoint = LOCALHOST + port + "clear";
+        restTemplate.delete(endpoint);
+    }
+
+    public List<String> listUsers() {
+        var endpoint = LOCALHOST + port + "/user/list";
+        String[] users = restTemplate.getForObject(endpoint, String[].class);
+        return List.of(users);
+    }
+
+    public String addUser(String name) {
+        var endpoint = LOCALHOST + port + "/user/add";
+        Map<String, Object> params = Map.of(USER_NAME, name);
+
+        Map<String, Object> response = castToMap(
+                restTemplate.postForObject(endpoint, params, Object.class)
+        );
+
+        Object outputName = response.get(USER_NAME);
+        Object outputToken = response.get(USER_TOKEN);
+        assertEquals(name, outputName);
+        assertNotNull(outputToken);
+
+        return outputToken.toString();
+    }
+
+    public List<String> addAllUsers(Collection<String> userNames) {
+        var userTokens = userNames
+                .stream()
+                .map(name -> addUser(name))
+                .collect(Collectors.toList());
+        return userTokens;
+    }
+
+    public void addFlit(String token, String content) {
+        var endpoint = LOCALHOST + port + "/flit/add";
+        Map<String, Object> params = Map.of(
+                USER_TOKEN, token,
+                CONTENT, content
+        );
+
+        Boolean response = restTemplate.postForObject(endpoint, params, Boolean.class);
+        assertTrue(response);
+    }
+
+    public List<Map<String, Object>> listAllFlits() {
+        var endpoint = LOCALHOST + port + "/flit/list";
+        List<Map<String, Object>> flits = castToMaps(
+                restTemplate.getForObject(endpoint, Object[].class)
+        );
+        return flits;
+    }
+
+    public List<Map<String, Object>> listFlitsByUser(String name) {
+        var endpoint = LOCALHOST + port + "/flit/list/" + name;
+        List<Map<String, Object>> flits = castToMaps(
+                restTemplate.getForObject(endpoint, Object[].class)
+        );
+        return flits;
+    }
+
+    public List<String> listSubscribers(String token) {
+        var endpoint = LOCALHOST + port + "/subscribers/list/" + token;
+        var subscribers = restTemplate.getForObject(endpoint, String[].class);
+        return List.of(subscribers);
+    }
+
+    public List<String> listPublishers(String token) {
+        var endpoint = LOCALHOST + port + "/publishers/list/" + token;
+        var subscribers = restTemplate.getForObject(endpoint, String[].class);
+        return List.of(subscribers);
+    }
+
+    public void subscribe(String subscriberToken, String publisherName) {
+        var endpoint = LOCALHOST + port + "/subscribe";
+        Map<String, Object> params = Map.of(
+                SUBSCRIBER_TOKEN, subscriberToken,
+                PUBLISHER_NAME, publisherName
+        );
+        restTemplate.postForObject(endpoint, params, Object.class);
+    }
+
+    public void subscribeAll(Collection<String> subscriberTokens, List<String> publisherNames) {
+        for (String publisherName : publisherNames) {
+            for (String subscriberToken : subscriberTokens) {
+                subscribe(subscriberToken, publisherName);
+            }
+        }
+    }
+
+    public void unsubscribe(String subscriberToken, String publisherName) {
+        var endpoint = LOCALHOST + port + "/unsubscribe";
+        Map<String, Object> params = Map.of(
+                SUBSCRIBER_TOKEN, subscriberToken,
+                PUBLISHER_NAME, publisherName
+        );
+        restTemplate.postForObject(endpoint, params, Object.class);
+    }
+
+    public void unsubscribeAll(Collection<String> subscriberTokens, List<String> publisherNames) {
+        for (String publisherName : publisherNames) {
+            for (String subscriberToken : subscriberTokens) {
+                unsubscribe(subscriberToken, publisherName);
+            }
+        }
+    }
+
+    public List<Map<String, Object>> listFlitsConsumedByUser(String token) {
+        var endpoint = LOCALHOST + port + "/flit/list/feed/" + token;
+        List<Map<String, Object>> flits = castToMaps(
+                restTemplate.getForObject(endpoint, Object[].class)
+        );
+        return flits;
+    }
+}
